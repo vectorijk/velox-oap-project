prestodb/presto

Add support for decimal inputs to min and max aggregate functions (#3755)
Summary:
The min and max aggregate functions need to be extended for decimal types. Added changes to create MinMax<> function objects  for decimal types in registerMinMaxAggregate.

Testing:
Unit tests for short and long decimals in MinMaxTest.cpp. Uses ability to generate decimal vector fuzzer from here: https://github.com/facebookincubator/velox/pull/3768.

For testing, the decimal value to decimal variant conversion was fixed.

Pull Request resolved: https://github.com/facebookincubator/velox/pull/3755

Reviewed By: kgpai, mbasmanova

Differential Revision: D43642441

Pulled By: Yuhta

fbshipit-source-id: 68873ff084d9f69a94c0cb88a2cf2cfb716c9fe6

velox/functions/prestosql/aggregates/MinMaxAggregates.cpp
@@ -74,6 +74,10 @@ class MinMaxAggregate : public SimpleNumericAggregate<T, T, T> {
     return sizeof(T);
   }
 
+  int32_t accumulatorAlignmentSize() const override {
+    return 1;
+  }
+
   void extractValues(char** groups, int32_t numGroups, VectorPtr* result)
       override {
     BaseAggregate::template doExtractValues<T>(
@@ -90,6 +94,15 @@ class MinMaxAggregate : public SimpleNumericAggregate<T, T, T> {
         });
   }
 };
+
+/// Override 'accumulatorAlignmentSize' for UnscaledLongDecimal values as it
+/// uses int128_t type. Some CPUs don't support misaligned access to int128_t
+/// type.
+template <>
+inline int32_t MinMaxAggregate<UnscaledLongDecimal>::accumulatorAlignmentSize()
+    const {
+  return static_cast<int32_t>(sizeof(UnscaledLongDecimal));
+}
 
 // Truncate timestamps to milliseconds precision.
 template <>
@@ -178,8 +191,11 @@ class MaxAggregate : public MinMaxAggregate<T> {
   }
 
  private:
-  static constexpr T kInitialValue_{MinMaxTrait<T>::lowest()};
+  static const T kInitialValue_;
 };
+
+template <typename T>
+const T MaxAggregate<T>::kInitialValue_ = MinMaxTrait<T>::lowest();
 
 template <typename T>
 class MinAggregate : public MinMaxAggregate<T> {
@@ -251,8 +267,11 @@ class MinAggregate : public MinMaxAggregate<T> {
   }
 
  private:
-  static constexpr T kInitialValue_{MinMaxTrait<T>::max()};
+  static const T kInitialValue_;
 };
+
+template <typename T>
+const T MinAggregate<T>::kInitialValue_ = MinMaxTrait<T>::max();
 
 class NonNumericMinMaxAggregateBase : public exec::Aggregate {
  public:
@@ -498,6 +517,10 @@ bool registerMinMax(const std::string& name) {
             return std::make_unique<TNumeric<Date>>(resultType);
           case TypeKind::INTERVAL_DAY_TIME:
             return std::make_unique<TNumeric<IntervalDayTime>>(resultType);
+          case TypeKind::LONG_DECIMAL:
+            return std::make_unique<TNumeric<UnscaledLongDecimal>>(resultType);
+          case TypeKind::SHORT_DECIMAL:
+            return std::make_unique<TNumeric<UnscaledShortDecimal>>(resultType);
           case TypeKind::VARCHAR:
           case TypeKind::ARRAY:
           case TypeKind::MAP:

vectorijk/velox-oap-project

velox/functions/prestosql/aggregates/MinMaxAggregates.cpp
@@ -73,6 +73,10 @@ class MinMaxAggregate : public SimpleNumericAggregate<T, T, T> {
     return sizeof(T);
   }
 
+  int32_t accumulatorAlignmentSize() const override {
+    return 1;
+  }
+
   void extractValues(char** groups, int32_t numGroups, VectorPtr* result)
       override {
     BaseAggregate::template doExtractValues<T>(
@@ -89,6 +93,15 @@ class MinMaxAggregate : public SimpleNumericAggregate<T, T, T> {
         });
   }
 };
+
+/// Override 'accumulatorAlignmentSize' for UnscaledLongDecimal values as it
+/// uses int128_t type. Some CPUs don't support misaligned access to int128_t
+/// type.
+template <>
+inline int32_t MinMaxAggregate<UnscaledLongDecimal>::accumulatorAlignmentSize()
+    const {
+  return static_cast<int32_t>(sizeof(UnscaledLongDecimal));
+}
 
 // Truncate timestamps to milliseconds precision.
 template <>
@@ -177,8 +190,11 @@ class MaxAggregate : public MinMaxAggregate<T> {
   }
 
  private:
-  static constexpr T kInitialValue_{MinMaxTrait<T>::min()};
+  static const T kInitialValue_;
 };
+
+template <typename T>
+const T MaxAggregate<T>::kInitialValue_ = MinMaxTrait<T>::min();
 
 template <typename T>
 class MinAggregate : public MinMaxAggregate<T> {
@@ -250,8 +266,11 @@ class MinAggregate : public MinMaxAggregate<T> {
   }
 
  private:
-  static constexpr T kInitialValue_{MinMaxTrait<T>::max()};
+  static const T kInitialValue_;
 };
+
+template <typename T>
+const T MinAggregate<T>::kInitialValue_ = MinMaxTrait<T>::max();
 
 class NonNumericMinMaxAggregateBase : public exec::Aggregate {
  public:
@@ -497,6 +516,10 @@ bool registerMinMaxAggregate(const std::string& name) {
             return std::make_unique<TNumeric<Date>>(resultType);
           case TypeKind::INTERVAL_DAY_TIME:
             return std::make_unique<TNumeric<IntervalDayTime>>(resultType);
+          case TypeKind::LONG_DECIMAL:
+            return std::make_unique<TNumeric<UnscaledLongDecimal>>(resultType);
+          case TypeKind::SHORT_DECIMAL:
+            return std::make_unique<TNumeric<UnscaledShortDecimal>>(resultType);
           case TypeKind::VARCHAR:
           case TypeKind::ARRAY:
           case TypeKind::MAP: