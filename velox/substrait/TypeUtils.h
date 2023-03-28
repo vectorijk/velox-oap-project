liujiayi771/velox

add decimal support in velox

velox/substrait/TypeUtils.h
@@ -81,6 +81,13 @@ struct RangeTraits<TypeKind::DATE> {
   using NativeType = int32_t;
 };
 
+template <>
+struct RangeTraits<TypeKind::SHORT_DECIMAL> {
+  using RangeType = common::BigintRange;
+  using MultiRangeType = common::BigintMultiRange;
+  using NativeType = int64_t;
+};
+
 #endif /* RANGETRAITS_H */
 
 } // namespace facebook::velox::substrait

vectorijk/velox-oap-project

velox/substrait/TypeUtils.h
@@ -81,6 +81,13 @@ struct RangeTraits<TypeKind::DATE> {
   using NativeType = int32_t;
 };
 
+template <>
+struct RangeTraits<TypeKind::SHORT_DECIMAL> {
+  using RangeType = common::ShortDecimalRange;
+  using MultiRangeType = common::ShortDecimalMultiRange;
+  using NativeType = UnscaledShortDecimal;
+};
+
 #endif /* RANGETRAITS_H */
 
 } // namespace facebook::velox::substrait