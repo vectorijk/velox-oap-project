11e9e6f8da841dd875eed049f72a9d66a18f3994
‘zhaozhenhui’ <zhenhui.zhao@intel.com>
2023-06-09 18:52:26 +0000

    fix tpcds q97

--- velox/functions/lib/IsNull.cpp
+++ velox/functions/lib/IsNull.cpp
@@ -16,7 +16,7 @@
 #include "velox/expression/DecodedArgs.h"
 #include "velox/expression/EvalCtx.h"
 #include "velox/expression/VectorFunction.h"
-
+#include <iostream>
 namespace facebook::velox::functions {
 namespace {
 
@@ -38,7 +38,7 @@ class IsNullFunction : public exec::VectorFunction {
     if (arg->isConstantEncoding()) {
       bool isNull = arg->isNullAt(rows.begin());
       auto localResult = BaseVector::createConstant(
-          BOOLEAN(), IsNotNULL ? !isNull : isNull, rows.end(), pool);
+          BOOLEAN(), IsNotNULL ? !isNull : isNull, rows.size(), pool);
       context.moveOrCopyResult(localResult, rows, result);
       return;
     }
@@ -46,7 +46,7 @@ class IsNullFunction : public exec::VectorFunction {
     if (!arg->mayHaveNulls()) {
       // No nulls.
       auto localResult = BaseVector::createConstant(
-          BOOLEAN(), IsNotNULL ? true : false, rows.end(), pool);
+          BOOLEAN(), IsNotNULL ? true : false, rows.size(), pool);
       context.moveOrCopyResult(localResult, rows, result);
       return;
     }
@@ -56,7 +56,7 @@ class IsNullFunction : public exec::VectorFunction {
       if constexpr (IsNotNULL) {
         isNull = arg->nulls();
       } else {
-        isNull = AlignedBuffer::allocate<bool>(rows.end(), pool);
+        isNull = AlignedBuffer::allocate<bool>(rows.size(), pool);
         memcpy(
             isNull->asMutable<int64_t>(),
             arg->rawNulls(),
@@ -65,8 +65,7 @@ class IsNullFunction : public exec::VectorFunction {
       }
     } else {
       exec::DecodedArgs decodedArgs(rows, args, context);
-
-      isNull = AlignedBuffer::allocate<bool>(rows.end(), pool);
+      isNull = AlignedBuffer::allocate<bool>(rows.size(), pool);
       memcpy(
           isNull->asMutable<int64_t>(),
           decodedArgs.at(0)->nulls(),
@@ -76,9 +75,14 @@ class IsNullFunction : public exec::VectorFunction {
         bits::negate(isNull->asMutable<char>(), rows.end());
       }
     }
-
+    std::cout << "((((((((( rows.size()" << rows.size()  << "rows.end()" << rows.end() << std::endl;
     auto localResult = std::make_shared<FlatVector<bool>>(
-        pool, BOOLEAN(), nullptr, rows.end(), isNull, std::vector<BufferPtr>{});
+        pool,
+        BOOLEAN(),
+        nullptr,
+        rows.size(),
+        isNull,
+        std::vector<BufferPtr>{});
     context.moveOrCopyResult(localResult, rows, result);
   }
 
