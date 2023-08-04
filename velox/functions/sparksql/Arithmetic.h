761221516817e28f591e9c0ed892719d77433cec
zhejiangxiaomai <zhenhui.zhao@intel.com>
2023-08-04 09:26:29 +0800

    move new round function to spark own namespace

--- velox/functions/sparksql/Arithmetic.h
+++ velox/functions/sparksql/Arithmetic.h
@@ -25,6 +25,35 @@
 
 namespace facebook::velox::functions::sparksql {
 
+/// Round function
+/// when AlwaysRoundNegDec is false, presto semantics is followed which does not
+/// round negative decimals for integrals and round it otherwise
+template <typename TNum, typename TDecimals, bool alwaysRoundNegDec = false>
+FOLLY_ALWAYS_INLINE TNum
+round(const TNum& number, const TDecimals& decimals = 0) {
+  static_assert(!std::is_same_v<TNum, bool> && "round not supported for bool");
+
+  if constexpr (std::is_integral_v<TNum>) {
+    if constexpr (alwaysRoundNegDec) {
+      if (decimals >= 0)
+        return number;
+    } else {
+      return number;
+    }
+  }
+  if (!std::isfinite(number)) {
+    return number;
+  }
+
+  double factor = std::pow(10, decimals);
+  static const TNum kInf = std::numeric_limits<TNum>::infinity();
+  if (number < 0) {
+    return (std::round(std::nextafter(number, -kInf) * factor * -1) / factor) *
+        -1;
+  }
+  return std::round(std::nextafter(number, kInf) * factor) / factor;
+}
+
 template <typename T>
 struct RemainderFunction {
   template <typename TInput>
