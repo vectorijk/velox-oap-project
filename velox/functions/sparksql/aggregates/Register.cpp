2bc34733e96cc6724a2fbddda4646d9e79030777
rui-mo <rui.mo@intel.com>
2023-08-14 00:48:17 +0000

    Spark sql sum agg function support decimal (5372)

--- velox/functions/sparksql/aggregates/Register.cpp
+++ velox/functions/sparksql/aggregates/Register.cpp
@@ -20,6 +20,7 @@
 #include "velox/functions/sparksql/aggregates/BitwiseXorAggregate.h"
 #include "velox/functions/sparksql/aggregates/BloomFilterAggAggregate.h"
 #include "velox/functions/sparksql/aggregates/SumAggregate.h"
+#include "velox/functions/sparksql/aggregates/DecimalSumAggregate.h"
 
 namespace facebook::velox::functions::aggregate::sparksql {
 
@@ -35,5 +36,6 @@ void registerAggregateFunctions(
   registerBloomFilterAggAggregate(prefix + "bloom_filter_agg");
   registerAverage(prefix + "avg", withCompanionFunctions);
   registerSum(prefix + "sum");
+  registerDecimalSumAggregate(prefix + "sum");
 }
 } // namespace facebook::velox::functions::aggregate::sparksql
