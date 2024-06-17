3cb370aa3c3e13d0b41f918ae10feb1cbc2fda05
Jia Ke <ke.a.jia@intel.com>
2024-06-18 02:11:04 +0800

    Add RowsStreamingWindowBuild to avoid OOM in Window operator (9025)

--- velox/exec/fuzzer/AggregationFuzzerBase.cpp
+++ velox/exec/fuzzer/AggregationFuzzerBase.cpp
@@ -405,6 +405,7 @@ velox::fuzzer::ResultOrError AggregationFuzzerBase::execute(
     const std::vector<exec::Split>& splits,
     bool injectSpill,
     bool abandonPartial,
+    bool supportRowsStreaming,
     int32_t maxDrivers) {
   LOG(INFO) << "Executing query plan: " << std::endl
             << plan->toString(true, true);
@@ -432,6 +433,10 @@ velox::fuzzer::ResultOrError AggregationFuzzerBase::execute(
           .config(core::QueryConfig::kAbandonPartialAggregationMinPct, "0")
           .config(core::QueryConfig::kMaxPartialAggregationMemory, "0")
           .config(core::QueryConfig::kMaxExtendedPartialAggregationMemory, "0");
+    }
+
+    if (supportRowsStreaming) {
+      builder.config(core::QueryConfig::kRowsStreamingWindowEnabled, "true");
     }
 
     if (!splits.empty()) {
@@ -524,6 +529,7 @@ void AggregationFuzzerBase::testPlan(
     bool injectSpill,
     bool abandonPartial,
     bool customVerification,
+    bool supportRowsStreaming,
     const std::vector<std::shared_ptr<ResultVerifier>>& customVerifiers,
     const velox::fuzzer::ResultOrError& expected,
     int32_t maxDrivers) {
@@ -532,6 +538,7 @@ void AggregationFuzzerBase::testPlan(
       planWithSplits.splits,
       injectSpill,
       abandonPartial,
+      supportRowsStreaming,
       maxDrivers);
   compare(actual, customVerification, customVerifiers, expected);
 }
