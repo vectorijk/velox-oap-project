oap-project/velox

[5962 ] Support struct column reading with different schemas (5962)

velox/dwio/parquet/reader/ParquetReader.cpp
@@ -24,6 +24,24 @@
 #include "velox/dwio/parquet/thrift/ThriftTransport.h"
 
 namespace facebook::velox::parquet {
+namespace {
+
+TypePtr adjustNameAsLowerCase(const TypePtr& type) {
+  if (auto rowTypePtr = asRowType(type)) {
+    std::vector<std::string> names;
+    names.reserve(rowTypePtr->names().size());
+    std::vector<TypePtr> types = rowTypePtr->children();
+    for (const auto& name : rowTypePtr->names()) {
+      std::string childName = name;
+      folly::toLowerAscii(childName);
+      names.emplace_back(childName);
+    }
+    return TypeFactory<TypeKind::ROW>::create(
+        std::move(names), std::move(types));
+  }
+  return type;
+}
+} // namespace
 
 /// Metadata and options for reading Parquet.
 class ReaderBase {
@@ -784,10 +802,13 @@ class ParquetRowReader::Impl {
     requestedType_ = options_.requestedType() ? options_.requestedType()
                                               : readerBase_->schema();
     columnReader_ = ParquetColumnReader::build(
-        requestedType_,
+        readerBase_->isFileColumnNamesReadAsLowerCase()
+            ? adjustNameAsLowerCase(requestedType_)
+            : requestedType_,
         readerBase_->schemaWithId(), // Id is schema id
         params,
-        *options_.getScanSpec());
+        *options_.getScanSpec(),
+        pool_);
     columnReader_->setFillMutatedOutputRows(
         options_.getRowNumberColumnInfo().has_value());
 

vectorijk/velox-oap-project

velox/dwio/parquet/reader/ParquetReader.cpp
@@ -47,6 +47,23 @@ TypePtr getUpdatedTableSchema(
       ? tableSchema
       : tableSchema->childAt(i);
 }
+
+
+TypePtr adjustNameAsLowerCase(const TypePtr& type) {
+  if (auto rowTypePtr = asRowType(type)) {
+    std::vector<std::string> names;
+    names.reserve(rowTypePtr->names().size());
+    std::vector<TypePtr> types = rowTypePtr->children();
+    for (const auto& name : rowTypePtr->names()) {
+      std::string childName = name;
+      folly::toLowerAscii(childName);
+      names.emplace_back(childName);
+    }
+    return TypeFactory<TypeKind::ROW>::create(
+        std::move(names), std::move(types));
+  }
+  return type;
+}
 } // namespace
 
 /// Metadata and options for reading Parquet.
@@ -943,10 +960,13 @@ class ParquetRowReader::Impl {
     requestedType_ = options_.requestedType() ? options_.requestedType()
                                               : readerBase_->schema();
     columnReader_ = ParquetColumnReader::build(
-        requestedType_,
+        readerBase_->isFileColumnNamesReadAsLowerCase()
+            ? adjustNameAsLowerCase(requestedType_)
+            : requestedType_,
         readerBase_->schemaWithId(), // Id is schema id
         params,
-        *options_.scanSpec());
+        *options_.scanSpec(),
+        pool_);
     columnReader_->setIsTopLevel();
 
     filterRowGroups();