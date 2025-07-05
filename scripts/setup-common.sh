e8ae14d56af475d72e29076f9b4f9333b2afbd44
Kai Jiang <jiangkai@gmail.com>
2025-07-04 23:50:09 -0700

    Update setup-common.sh

--- scripts/setup-common.sh
+++ scripts/setup-common.sh
@@ -248,6 +248,7 @@ function install_faiss {
       -DFAISS_ENABLE_GPU_TESTS=OFF \
       -DFAISS_ENABLE_BENCHMARKS=OFF -DCMAKE_CXX_FLAGS="-Xpreprocessor -fopenmp -I/usr/include" \
       -DOpenMP_CXX_LIB_NAMES="omp" \
+      -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I/usr/include" \
       -DOpenMP_omp_LIBRARY=/usr/lib/llvm-11/lib/libomp.so
   fi
 }
