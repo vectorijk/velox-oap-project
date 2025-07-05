c9ecef9bae8f80b161017a4ddc9f706835cbae5b
Kai Jiang <jiangkai@gmail.com>
2025-07-05 00:39:30 -0700

    Update setup-common.sh

--- scripts/setup-common.sh
+++ scripts/setup-common.sh
@@ -246,9 +246,9 @@ function install_faiss {
       -DFAISS_ENABLE_PYTHON=OFF \
       -DFAISS_ENABLE_REMOTE=OFF \
       -DFAISS_ENABLE_GPU_TESTS=OFF \
-      -DFAISS_ENABLE_BENCHMARKS=OFF -DCMAKE_CXX_FLAGS="-stdlib=libc++ -nostdinc++ -Xpreprocessor -fopenmp -I/usr/lib/llvm-11/lib/clang/11.0.1/include" \
+      -DFAISS_ENABLE_BENCHMARKS=OFF -DCMAKE_CXX_FLAGS="-stdlib=libc++ -nostdinc++ -Xpreprocessor -fopenmp  -I/usr/lib/llvm-11/lib/clang/11.0.1/include  -fPIC -stdlib=libc++ -isystem /usr/lib/llvm-11/include/c++/v1 -isystem /usr/lib/llvm-11/lib/clang/11.0.1/include -isystem /usr/include" \
       -DOpenMP_CXX_LIB_NAMES="omp" \
-      -DOpenMP_CXX_FLAGS="-stdlib=libc++ -nostdinc++ -Xpreprocessor -fopenmp -I/usr/lib/llvm-11/lib/clang/11.0.1/include" \
+      -DOpenMP_CXX_FLAGS="-stdlib=libc++ -nostdinc++ -Xpreprocessor -fopenmp  -I/usr/lib/llvm-11/lib/clang/11.0.1/include  -fPIC -stdlib=libc++ -isystem /usr/lib/llvm-11/include/c++/v1 -isystem /usr/lib/llvm-11/lib/clang/11.0.1/include -isystem /usr/include" \
       -DOpenMP_omp_LIBRARY="/usr/lib/llvm-11/lib/libomp.so"
   fi
 }
